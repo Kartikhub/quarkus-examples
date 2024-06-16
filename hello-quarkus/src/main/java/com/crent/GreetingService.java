7250433c7db277abb3e231617cd4920a3baaedbe
Kartikhub <sharmakartik66@gmail.com>
2024-06-17 00:48:21 +0530

    moved all projects in same directory

---
+++ hello-quarkus/src/main/java/com/crent/GreetingService.java
@@ -0,0 +1,10 @@
+package com.crent;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+    public String greeting(String name) {
+        return "hello "  + name;
+    }
+}
